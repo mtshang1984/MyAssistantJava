9b6292262e8c51e4d56593e4cae94469b5835a58
mtshang1984 <mtshang1984@163.com>
2016-01-16 22:07:31 +0800

    新增一些分类，等增加模块代码

--- src/MyAssistant.java
+++ src/MyAssistant.java
@@ -1,3 +1,17 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import Assistant.AissitantStartup;
+import Assistant.AssistantLife;
+import Requirement.RequirementsAnalysis;
+import SearchEngine.SearchEngine;
+
+
 public class MyAssistant {
 
 	public static void main(String[] args) {
@@ -39,6 +53,38 @@ public class MyAssistant {
 		requirements_analysis.ShowRequirement();
 		
 		if (Keyword.isEmpty()) {
+			File config_file=new File("config.ini");
+			if(!config_file.exists()){
+				System.out.println("找不到config.ini文件！");
+				System.exit(0);
+			}else
+			{
+				try {
+					FileInputStream file_input_stream=new FileInputStream(config_file); 
+					InputStreamReader input_stream_reader;
+					try {
+						input_stream_reader = new InputStreamReader(file_input_stream, "GBK");
+						BufferedReader input_stream=new BufferedReader(input_stream_reader);
+						String tempString = null;
+						try {
+							while((tempString=input_stream.readLine())!= null){
+								System.out.println(tempString);
+							}
+							input_stream_reader.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} 
+					} catch (UnsupportedEncodingException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} 
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+				
 			AssistantLife.Today();
 			AissitantStartup.Today();
 			
